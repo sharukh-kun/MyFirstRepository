d06b4239c8ae66c077df32e53381a858aa348d7a
sharukh-kun <sha786rukhm@gmail.com>
2025-06-30 11:16:22 +0530

    updated to additon of four numbers

--- secondpython.py
+++ secondpython.py
@@ -1,6 +1,7 @@
 #this is my second file made from a branch
-#Add two numbers
+#Add three numbers
 a = int(input("enter first number"))
 b = int(input("enter second number"))
-sum = a+b
+c= int(input("Enter the third number"))
+sum = a+b+c
 print(sum)
